ae14ef3c9a27dc706dd5ea488fe6faa27558cea4
Esha <shuminnahar8di@gmail.com>
2026-05-13 08:04:14 +0600

    product handling to read from database and improve response structure

--- src/controller/controller.products.ts
+++ src/controller/controller.products.ts
@@ -1,12 +1,18 @@
-import type { IncomingMessage, ServerResponse } from "node:http"
+import type { IncomingMessage, ServerResponse } from "node:http";
+import { readProduct } from "../service/product.service";
 
-import product from "../database/db.json"
-
-export function products(req:IncomingMessage,res:ServerResponse) {
-     res.writeHead(200,{
-            "content-type":"application/json"
-        })
-        res.end(JSON.stringify({message:"this is root",data:product}))
-
-
+export function products(req: IncomingMessage, res: ServerResponse) {
+  const url = req.url;
+  const method = req.method;
+  console.log(url?.split);
+  if (url === "/product" && method === "GET") {
+    const productList = readProduct();
+    res.writeHead(200, {
+      "content-type": "application/json",
+    });
+    
+    res.end(JSON.stringify({ message: "this is root", data: productList }));
+  }
+  
+  
 }
--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -1,7 +1,9 @@
 
 import path from "node:path"
+import fs from "node:fs"
 
 export function readProduct(){
     const filepath=path.join(process.cwd(),"./src/database/db.json")
-    console.log(filepath)
+    const products=fs.readFileSync(filepath,"utf-8")
+    return JSON.parse(products)
 }
