e256e53df28e7ab5b6215c151d9f9d4e74f02e4b
esha8di <shuminnahar8di@gmail.com>
2026-05-13 11:53:26 +0600

    product and single get product with id done

--- src/controller/controller.products.ts
+++ src/controller/controller.products.ts
@@ -4,15 +4,34 @@ import { readProduct } from "../service/product.service";
 export function products(req: IncomingMessage, res: ServerResponse) {
   const url = req.url;
   const method = req.method;
-  console.log(url?.split);
+
+  const urlParts = url?.split("/");
+
+  const id = urlParts && urlParts[1] === "product" ? Number(urlParts[2]) : null;
+
   if (url === "/product" && method === "GET") {
     const productList = readProduct();
     res.writeHead(200, {
       "content-type": "application/json",
     });
-    
+
     res.end(JSON.stringify({ message: "this is root", data: productList }));
+  } else if (method === "GET" && id !== null) {
+    const productList = readProduct();
+    const productWithId=productList.filter((product) => product.id === id);
+    if(productWithId.length>0){
+      res.writeHead(200, {
+        "content-type": "application/json",
+      });
+      res.end(JSON.stringify({ message: "data retrieve successfully", data: productWithId }));
+    
   }
-  
-  
+  else{
+  res.writeHead(404, {
+    "content-type": "application/json",
+  });
+  res.end(JSON.stringify({ message: "this data is not available" }));
+}
+}
+
 }
