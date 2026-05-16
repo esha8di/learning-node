0eefb9155e3b04b5dab746d55302f136e8a577e7
Esha <shuminnahar8di@gmail.com>
2026-05-16 09:56:06 +0600

    put method started

--- src/controller/controller.products.ts
+++ src/controller/controller.products.ts
@@ -53,5 +53,14 @@ else if (url === "/product" && method === "POST") {
   
   
 }
+else if(method==="PUT" && id!==null){
+   const productList = readProduct();
+  const productId=productList.findIndex((product:IProduct)=>product.id==id)
+  console.log(productId)
+  res.writeHead(200, {
+    "content-type": "application/json",
+  });
+  res.end(JSON.stringify({ message: "data receive successfully" }));
+}
 
 }
